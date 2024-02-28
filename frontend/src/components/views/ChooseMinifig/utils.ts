2f1a34c8d3541567189903d17ec6d21c78e0d834
maciekjasinski <maciek.mj00@gmail.com>
2024-02-28 14:03:45 +0100

    fix: missing redirect, lint

--- frontend/src/components/views/ChooseMinifig/utils.ts
+++ frontend/src/components/views/ChooseMinifig/utils.ts
@@ -5,12 +5,15 @@ export const fetchMinifigs = () => {
   return axios
     .get(`https://rebrickable.com/api/v3/lego/minifigs/?key=${import.meta.env.VITE_REBRICKABLE_API_KEY}&page_size=3`)
     .then((res) => {
-      return res.data.results.map((each: { name: string; set_img_url: string | null; set_url: string }) => {
-        return {
-          name: each.name,
-          picture: each.set_img_url || null,
-          detailsUrl: each.set_url,
-        };
-      }) as MinifigCardProps[];
+      return res.data.results.map(
+        (each: { name: string; set_img_url: string | null; set_url: string; set_num: number }) => {
+          return {
+            name: each.name,
+            picture: each.set_img_url || null,
+            detailsUrl: each.set_url,
+            setId: each.set_num,
+          };
+        },
+      ) as MinifigCardProps[];
     });
 };
