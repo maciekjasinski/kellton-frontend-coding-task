e855bf48e9433e074b003107e6c91a68c9177d1c
maciekjasinski <maciek.mj00@gmail.com>
2024-02-28 17:36:08 +0100

    feat: part card

---
+++ frontend/src/types/minifig.ts
@@ -0,0 +1,12 @@
+export type MinifigType = {
+  name: string;
+  picture: string | null;
+  detailsUrl: string;
+  setId: string;
+}
+
+export type MinifigPartType = {
+  id: string;
+  name: string;
+  picture: string | null;
+}
