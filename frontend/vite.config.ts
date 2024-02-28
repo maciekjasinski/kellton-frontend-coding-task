8604cb677444fddc519f694dd47337b35edce456
maciekjasinski <maciek.mj00@gmail.com>
2024-02-28 06:58:58 +0100

    feat: import alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@common': path.resolve(__dirname, 'src/components/common'),
+      '@modules': path.resolve(__dirname, 'src/components/modules'),
+      '@views': path.resolve(__dirname, 'src/components/views'),
+    }
+  }
 });
